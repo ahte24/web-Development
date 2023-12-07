64855fb5e31abd34f1abddfc7f33bf8fbdc3e8f0
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-07 23:49:10 +0530

    intro to js

---
+++ video 54 Js/new.js
@@ -0,0 +1,4 @@
+console.log("hello wrold")
+console.log("Hey i am console.")
+console.error("hello world")
+console.error("bhago yaar")  
---
+++ video 54 Js/script.js
@@ -0,0 +1,19 @@
+alert("hello wrold")
+
+console.log("Hey i am console.")
+console.error("hello world")
+console.error("bhago yaar")
+
+var a = prompt("Enter your number:  ")
+
+console.log("your number is: " + a)
+
+var isTrue = confirm("Are you sure you want to leave this page and agreed to blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting.")
+}else{
+    console.log("computer is not blasting.")
+}
+
+
