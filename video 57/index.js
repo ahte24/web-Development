103fa5f3382e2ba1e164bffb381d78c2240b8769
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-08 23:11:43 +0530

    multiple for loops, while loop and do while loop

---
+++ video 57/index.js
@@ -0,0 +1,30 @@
+console.log("i am a tutorial on loops");
+
+// for (let i = 1; i <= 100; i++) console.log(i);
+
+// let obj = {
+// 	name: ": Ahtesham",
+// 	role: ": SDE",
+// 	company: ": Goodle",
+// };
+
+// for (const key in obj) {
+// 	const element = obj[key];
+// 	console.log(key, element);
+// }
+
+// for (const c of "ahtesham") {
+//     console.log(c);
+// }
+
+let i = 0;
+
+// while (i <= 7) {
+// 	console.log(i);
+// 	i++;
+// }
+
+do {
+	console.log(i);
+	i++;
+} while (i < 0);
