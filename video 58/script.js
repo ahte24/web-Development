3c2b018a2b157c1525b0d86335558537298f826e
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-09 22:15:35 +0530

    functions

---
+++ video 58/script.js
@@ -0,0 +1,24 @@
+function nice(name) {
+	console.log("Hey " + name + " you are nice");
+	console.log("Hey " + name + " you are good");
+	console.log("Hey " + name + " your dedication is nice");
+	console.log("Hey " + name + " your tshirt are nice");
+}
+
+nice("ahtesham");
+nice("harry");
+
+function sum(a, b, c = 4) {
+	return a + b + c;
+}
+
+result = sum(4, 5);
+console.log(result);
+
+// Arror function
+
+const func1 = (az) => {
+	console.log("hello my number is: ", az);
+};
+
+func1(9114111987);
