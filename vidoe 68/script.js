1e347d664694af24aa1155f1a0be9744215be98d
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-14 23:12:31 +0530

    JavaScript - Selecting by Ids, Classes, and More

---
+++ vidoe 68/script.js
@@ -0,0 +1,53 @@
+// ----------------------get elements by class---------------------//
+
+// console.log("DOM Tutorial.");
+// let boxes = document.body.getElementsByClassName("box");
+
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor = "green";
+
+// ---------------------------get element by id--------------------//
+
+// let boxes = document.getElementById("red");
+// console.log(boxes);
+
+// boxes.style.backgroundColor = "red";
+
+// ------------------------ Query Selector-------------------------//
+
+// let box = document.querySelector(".box");
+// box.style.backgroundColor = "green";
+
+// ---------------------- Query Selector All-----------------------//
+
+// let boxes = document.querySelectorAll(".box");
+// console.log(boxes);
+
+// boxes.forEach((e) => {
+// 	e.style.backgroundColor = "green";
+// 	console.log(e);
+// });
+
+// for (let i = 0; i < boxes.length; i++) {
+// 	boxes[i].style.backgroundColor = "aqua";
+// }
+
+// ------------------------get element by TagName------------------//
+
+// let boxes = document.getElementsByTagName("div");
+// console.log(boxes);
+
+// ---------------------------------Matches---------------------------//
+
+// console.log(boxes[4].matches("#red"));
+
+// ---------------------------------Matches---------------------------//
+
+// console.log(boxes[4].closest("html"));
+
+// ---------------------------------Contains--------------------------//
+
+// console.log(
+// 	document.querySelector(".container").contains(document.querySelector("body"))
+// );
