94ede4137f08111541eeca9d8e0cb050db557923
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-18 00:31:37 +0530

    Inserting, Removing Elements and more using JavaScript

---
+++ video 71/script.js
@@ -0,0 +1,19 @@
+let box = document.querySelector(".box");
+// let box2 = document.querySelector(".container");
+// console.log(box)
+// console.log(box.innerHTML="Hey i am ahteshamuz zaman");
+// console.log(box.hasAttribute("style"));
+// console.log(box.setAttribute("style", "display: inline"));
+// console.log(box.getAttribute("style"));
+// console.log(box.removeAttribute("style"));
+console.log(box.attributes);
+// console.log(box.remove("style"));
+// console.log(box.outerHTML);
+// console.log(box.tagName);
+// console.log(box.nodeName);
+// console.log(box.textContent);
+// console.log(box.hidden=true);
+// console.log(box2.outerHTML);
+// console.log(box2);
+
+console.log(box.dataset);
