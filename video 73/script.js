11c7eeafa2a36527a500eda2cfeab883a9e217a0
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-22 00:44:59 +0530

    JavaScript Exercise 13 - Dynamic Website Builder

---
+++ video 73/script.js
@@ -0,0 +1,12 @@
+console.log("Challange Accepted.");
+
+// Function to change the image
+function changeImage() {
+	let imageElement = document.getElementById("img");
+	imageElement.src =
+		"https://i.ytimg.com/vi/ognrhoi0C-w/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDgeqfhd6yCgUzQOVgxyXHRArCXWQ";
+	let h3 = document.getElementById("h3");
+	h3.textContent =
+		"CSS Object-fit and Object-cover | Sigma Web Development Course - Tutorial #49";
+}
+
