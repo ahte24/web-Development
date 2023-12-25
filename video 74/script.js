702f16e37fa8b97e15f6642fee2fc0a8fd47dc55
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2023-12-25 21:04:39 +0530

    Events, Event Bubbling of JavaScript

---
+++ video 74/script.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+btn.addEventListener("dblclick", () => {
+	// alert("I was clicked.");
+	document.querySelector(".box").innerHTML =
+		"<b>you have been clicked.</b> enjoy";
+});
+btn.addEventListener("contextmenu", () => {
+	alert("Pro coder dont copy.");
+});
+btn.addEventListener("keydown", (e) => {
+	console.log(e.key, e.keyCode);
+});
