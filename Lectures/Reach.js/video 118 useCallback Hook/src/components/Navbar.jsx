c0c9b12fdaa1c8aadc9b5058f0571a7a6e8e016d
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2024-03-11 13:05:12 +0530

    useCallback Hook

---
+++ Lectures/Reach.js/video 118 useCallback Hook/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { memo } from "react";
+
+const Navbar = ({ adjective, getAdjective }) => {
+	console.log("Navbar is rendering.....");
+	return (
+		<div>
+			I am a {adjective} Navbar..
+			<button
+				onClick={() => {
+					getAdjective();
+				}}
+			>
+				{getAdjective()}
+			</button>
+		</div>
+	);
+};
+
+export default memo(Navbar);
